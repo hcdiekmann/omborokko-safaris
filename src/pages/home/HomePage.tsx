62bae996bd20c1899521f3ccf2926a7f3257fa1d
hcdiekmann <hcdiekmann@hotmail.com>
2023-06-20 09:48:42 +0200

    adds carousel to homepage

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,5 +1,72 @@
-import { Title } from '@mantine/core';
+import { Title, rem } from '@mantine/core';
+import { useRef } from 'react';
+import Autoplay from 'embla-carousel-autoplay';
+import { Carousel } from '@mantine/carousel';
 
 export const HomePage = (): JSX.Element => {
-  return <Title order={2}>Home Page</Title>;
+  const autoplay = useRef(Autoplay({ delay: 4000 }));
+  return (
+    <Carousel
+      maw={750}
+      mx='auto'
+      withIndicators
+      height={500}
+      plugins={[autoplay.current]}
+      onMouseEnter={autoplay.current.stop}
+      onMouseLeave={autoplay.current.reset}
+      styles={{
+        indicator: {
+          width: rem(12),
+          height: rem(4),
+          transition: 'width 250ms ease',
+
+          '&[data-active]': {
+            width: rem(40),
+          },
+        },
+        control: {
+          '&[data-inactive]': {
+            opacity: 0,
+            cursor: 'default',
+          },
+        },
+      }}
+    >
+      <Carousel.Slide>
+        <img
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          src='/pictures/webp/Campsite 1__9_11zon.webp'
+          alt='Slide 1'
+        />
+      </Carousel.Slide>
+      <Carousel.Slide>
+        <img
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          src='/pictures/webp/B&B Outside patio with view_6_11zon.webp'
+          alt='Slide 2'
+        />
+      </Carousel.Slide>
+      <Carousel.Slide>
+        <img
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          src='/pictures/webp/B&B Family Unit Outside patio_3_11zon.webp'
+          alt='Slide 3'
+        />
+      </Carousel.Slide>
+      <Carousel.Slide>
+        <img
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          src='/pictures/webp/B&B Family unit Room 1_4_11zon.webp'
+          alt='Slide 4'
+        />
+      </Carousel.Slide>
+      <Carousel.Slide>
+        <img
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          src='/pictures/webp/B&B Outside patio with view_6_11zon.webp'
+          alt='Slide 5'
+        />
+      </Carousel.Slide>
+    </Carousel>
+  );
 };
