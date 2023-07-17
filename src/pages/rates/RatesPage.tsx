4c0361ae621241cfd7aca4938e4cb856abebac9c
hcdiekmann <hcdiekmann@hotmail.com>
2023-07-18 08:14:31 +1200

    add page card placeholders

--- src/pages/rates/RatesPage.tsx
+++ src/pages/rates/RatesPage.tsx
@@ -1,9 +1,11 @@
-import { Title, Container } from '@mantine/core';
+import { Title, Container, Card } from '@mantine/core';
 
 export const RatesPage = (): JSX.Element => {
   return (
-    <Container>
-      <Title order={2}>Rates Page</Title>
+    <Container mt={100}>
+      <Card>
+        <Title order={2}>Our Rates</Title>
+      </Card>
     </Container>
   );
 };
