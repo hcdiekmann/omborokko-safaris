50baef9b5e5549272b35136862d59ee1011c44a6
hcdiekmann <hcdiekmann@hotmail.com>
2023-07-20 10:51:19 +1200

    adds price table

--- src/pages/rates/RatesPage.tsx
+++ src/pages/rates/RatesPage.tsx
@@ -1,10 +1,42 @@
-import { Title, Container, Card } from '@mantine/core';
+import { Title, Container, Card, Space, Text, Badge } from '@mantine/core';
+import { PriceTable } from './components/PriceTable';
+
+const campingElements = [
+  { id: 1, description: 'Adult per person per night ', price: 'N$ 250' },
+  { id: 2, description: 'Children (16 or under)', price: 'N$ 150' },
+  { id: 3, description: 'Extra firewood bag', price: 'N$ 50' },
+];
+
+const bedElements = [
+  { id: 1, description: 'Adult per person per night', price: 'N$ 850' },
+];
 
 export const RatesPage = (): JSX.Element => {
   return (
     <Container mt={100}>
       <Card>
-        <Title order={2}>Our Rates</Title>
+        <Title order={2} align='center'>
+          Our Rates
+        </Title>
+        <Title order={3}>Camping</Title>
+        <PriceTable elements={campingElements} />
+        <Space h='lg' />
+        <Title order={3}>Bed & Breakfast</Title>
+        <PriceTable elements={bedElements} />
+        <Card>
+          <Badge color='yellow' variant='light'>
+            Please Note
+          </Badge>
+
+          <Text fz='sm' c='dimmed'>
+            {' '}
+            - We currently only accept cash payments.
+          </Text>
+          <Text fz='sm' c='dimmed'>
+            {' '}
+            - All payments shall be made upon arrival.
+          </Text>
+        </Card>
       </Card>
     </Container>
   );
