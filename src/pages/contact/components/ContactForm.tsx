f17851e086b0abf7563ff3c91c10ac98cf97a3c3
hcdiekmann <hcdiekmann@hotmail.com>
2023-07-20 09:59:05 +1200

    reset date picker on submit

--- src/pages/contact/components/ContactForm.tsx
+++ src/pages/contact/components/ContactForm.tsx
@@ -55,7 +55,7 @@ export const ContactForm = (): JSX.Element => {
         body: JSON.stringify({
           ...values,
           startDate: dateRange[0]
-            ? dateRange[0].toISOString().split('T')[0]
+            ? dateRange[0].toISOString().split('T')[0] // Convert to ISO string and remove time
             : null,
           endDate: dateRange[1]
             ? dateRange[1].toISOString().split('T')[0]
@@ -83,6 +83,7 @@ export const ContactForm = (): JSX.Element => {
       // Set loading back to false when the request ends,
       // whether it was successful or not
       setIsLoading(false);
+      setDateRange([null, null]);
     }
   };
 
